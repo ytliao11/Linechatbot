510a0e0ba24b287e4fdfaab28cd5991f82bfa0bf
ytliao11 <165812657+ytliao11@users.noreply.github.com>
2024-04-24 17:48:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,14 @@ def handle_message(event):
         else:
             reply_message = "無法獲取數據。"
     elif user_message == "殘障車位":
-        reply_message = "目前沒有殘障車位的資料。"
+        response = requests.get('https://script.google.com/macros/s/AKfycbw_A5VQZro0lXfldSyZkc4BY1n0rFIpqUIgy_wt7IEq0BGca37wdTL3kr2egmFJSqxOJg/exec')
+        data = response.json()
+        if data['status'] == 'success':
+            last_record = data['data']
+            remaining_slots = last_record[0][2] 
+            reply_message = f"殘障車位剩餘:{remaining_slots}個空位"
+        else:
+            reply_message = "無法獲取數據。"
     else:
         reply_message = "請回答「一般車位」或「殘障車位」。"
     line_bot_api.reply_message(
